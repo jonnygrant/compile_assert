58023c510f706bb924013652c593e11b7309d64d
jonnygrant <jg@jguk.org>
2023-09-26 23:10:08 +0100

    Add files via upload

---
+++ main4.c
@@ -0,0 +1,18 @@
+// gcc -O2 -Wall -o main4 main4.c
+
+// demonstrate compile_assert to checking all indices into an array are within bounds
+
+#include "compile_assert.h"
+#include <stdio.h>
+
+int main()
+{
+    const size_t buf_size = 4;
+    char buf[buf_size];
+
+    for(size_t i = 0; i != 5; ++i)
+    {
+        compile_assert(i < buf_size, "check buf index");  // will fire, as out of bounds
+        buf[i] = 3;
+    }
+}
---
+++ main5.c
@@ -0,0 +1,25 @@
+// gcc -O2 -Wall -o main5 main5.c
+
+// demonstrate compile_assert to checking all indices into an array are within bounds
+
+#include "compile_assert.h"
+#include <stdio.h>
+
+int main()
+{
+    const size_t buf_size = 2;
+    size_t buf[buf_size];
+    buf[0] = 2; // fires because these should be '1';
+    buf[1] = 2;
+
+    char second[buf_size];
+    second[0] = 'a';
+
+    for(size_t i = 0; i != buf_size; ++i)
+    {
+        compile_assert(buf[i] < buf_size, "check buf index");  // this will fire because '2' and '2' are out of bounds
+        second[buf[i]] = 100;
+    }
+
+    printf("%d\n", second[0]);
+}
