b2bbb4183bb71f7b85fa9c0ce94a5479b19a4d68
jonnygrant <jg@jguk.org>
2023-09-26 23:09:43 +0100

    Update compile_assert.h

--- compile_assert.h
+++ compile_assert.h
@@ -1,20 +1,32 @@
 #ifndef COMPILE_ASSERT_H
 #define COMPILE_ASSERT_H
 
+#include <stdbool.h>
+
 // Utilize GCC attribute error as part of an optimized build to stop when conditions are not met
 // This is at build time, by the compiler, when it does redundant code removal, aka dead code removal.
 
 // Implemented in C, it can be used in C++ projects as well.
 
+// Few other ideas, to check float range etc
+
+// The description isn't logged yet, it is at least a note to the programmer
+
 #ifdef __OPTIMIZE__
-#define compile_assert(condition, description) {if(!condition) { _stop_compile();}}
+void _stop_compile() __attribute ((error("build stopped")));
+void * _stop_compile2(void * p) __attribute ((error("build stopped")));
+
+inline void _compile_assert(bool condition, const char * const description)
+{
+    (void)description;
+
+    if(!condition) _stop_compile();
+}
+
+#define compile_assert(condition, description) _compile_assert(condition, description)
 #else
 #define compile_assert(condition, description)
 #endif
-
-void _stop_compile() __attribute ((error("build stopped")));
-
-void * _stop_compile2(void * p) __attribute ((error("build stopped")));
 
 #ifdef __OPTIMIZE__
 #define compile_never_null(ptr) ptr?ptr:_stop_compile2((void*)ptr)
