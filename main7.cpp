d94ce9534effa84d25246927bffb44fdfb2771ce
jonnygrant <jg@jguk.org>
2023-09-26 23:33:24 +0100

    Update main7.cpp

--- main7.cpp
+++ main7.cpp
@@ -1,6 +1,7 @@
 // g++ -O2 -Wall -o main7 main7.cpp
 
-// Try std::string
+// Try compile_assert with a C++ STL container std::string
+// Note: There is some strange interoperation, the 2nd always fails, unless the 1st is commented out
 
 #include "compile_assert.h"
 #include <stdio.h>
@@ -11,7 +12,6 @@ int main()
 {
     const std::string str("hello");
 
-    // there is some strange interoperation, the 2nd always fails, unless the 1st is commented out
     //compile_assert(str[0] == 'a', "Must be 'h'"); // change to 'h' to pass
     compile_assert(str.size() == 6, "Must be 5");
     printf("%zu\n", str.size());
