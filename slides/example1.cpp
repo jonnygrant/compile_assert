bcc163774a20a019ce6b17e7420762619c59f37e
Jonathan Grant <jg@jguk.org>
2026-03-07 01:45:15 +0000

    add slides

---
+++ slides/example1.cpp
@@ -0,0 +1,18 @@
+// g++ -I.. -D__ENABLE_COMPILE_ASSERT__ -O2 -Wall -c -o example1.o example1.cpp
+
+#include "compile_assert.h"
+
+// External API
+void add_to_log(const char * p);
+
+static void my_log(const char * p)
+{
+  compile_assert(p != nullptr, "may be null");
+  add_to_log(p);
+}
+
+
+void app_api(const char * ptr)
+{
+  my_log(ptr);
+}
