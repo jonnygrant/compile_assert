c142e9735d3e85a7ea21ce7ab8c96c9d9fac8e36
Jonathan Grant <jg@jguk.org>
2026-07-18 22:46:09 +0100

    C example ownership

---
+++ experiments/ownership/ownership1.c
@@ -0,0 +1,33 @@
+
+// Demonstrate assigning ownership of an object, upon which it can no longer be used by
+// the original object
+
+#include "float_container.h"
+
+// Example only works by copying and passing a reference
+extern void ownership2(float_container * value2);
+
+int main()
+{
+    float_container value;
+
+    float_container_init(&value);
+
+    float_container_set(&value, 3.14f);
+    __builtin_printf("%6.2f\n", float_container_get(&value));
+
+    float_container value2;
+
+    float_container_init(&value2);
+    float_container_ownership(&value2, &value);
+
+    compile_assert(value.ptr == nullptr, "error nullptr");
+
+    //value2 = value;
+    // Takes ownership of the allocation
+    ownership2(&value2);
+
+    // fail to compile
+    // comment out, and see warning disapears
+    __builtin_printf("after ownership %6.2f\n", float_container_get(&value));
+}
---
+++ experiments/ownership/ownership2.c
@@ -0,0 +1,12 @@
+
+// Demonstrate assigning ownership of an object, upon which it can no longer be used by
+// the original object
+
+#include "float_container.h"
+
+void ownership2(float_container * value2)
+{
+    // fail to compile
+    // comment out, and see error disapears
+    __builtin_printf("%6.2f\n", float_container_get(value2));
+}
